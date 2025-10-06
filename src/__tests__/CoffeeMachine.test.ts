b274d2a2c295f51fa32014fcc5e4a4e191578fb8
DamianDudra <159533662+DamianDudra@users.noreply.github.com>
2025-10-06 17:54:07 +0000

    dokoncene

--- src/__tests__/CoffeeMachine.test.ts
+++ src/__tests__/CoffeeMachine.test.ts
@@ -2,117 +2,144 @@ import { Drink } from '../Drink';
 import { CoffeeMachine } from '../CoffeeMachine';
 
 describe("CoffeeMachine", () => {
-  it("should serve coffee if exact money inserted", () => {
-    const machine = new CoffeeMachine();
-
-    const drink = new Drink("Coffee", 2, false, 0, "small");
-
-    expect(machine.serve(drink, 2, false, 10)).toBe("Serving Coffee (small)");
-  });
   it("should give 5th drink for free if you have loyalty card", () => {
-    const machine = new CoffeeMachine();
-    
     //Arrange
+    const insertedmoney=0
+    const loyaltycard=true
+    const machine = new CoffeeMachine();
     const drink = new Drink("Coffee", 2, false, 0, "small");
-    machine.serve(drink, 2, false, 10)
-    machine.serve(drink, 2, false, 10)
-    machine.serve(drink, 2, false, 10)
-    machine.serve(drink, 2, false, 10)
-
-    expect(machine.serve(drink, 0, true, 10)).toBe("Serving Coffee(small)");
+    //Act
+    machine.serve(drink, 2, loyaltycard, 10);
+    machine.serve(drink, 2, loyaltycard, 10);
+    machine.serve(drink, 2, loyaltycard, 10);
+    machine.serve(drink, 2, loyaltycard, 10);
+    //Act and Assert
+    expect(machine.serve(drink, insertedmoney, loyaltycard, 10)).toBe("Serving Coffee (small)");
   });
+
   it("shouldn't give 5th drink for free if you have loyalty card and a large drink", () => {
-    const machine = new CoffeeMachine();
     //Arrange
+    const insertedmoney=3
+    const loyaltycard=true
+    const machine = new CoffeeMachine();
     const drink = new Drink("Coffee", 2, false, 0, "large");
-    machine.serve(drink, 3, false, 10)
-    machine.serve(drink, 3, false, 10)
-    machine.serve(drink, 3, false, 10)
-    machine.serve(drink, 3, false, 10)
-
-    expect(machine.serve(drink, 3, true, 10)).toBe("Serving Coffee (large)");
+    //Act
+    machine.serve(drink, 3, loyaltycard, 10);
+    machine.serve(drink, 3, loyaltycard, 10);
+    machine.serve(drink, 3, loyaltycard, 10);
+    machine.serve(drink, 3, loyaltycard, 10);
+    //Act and Assert
+    expect(machine.serve(drink, insertedmoney, loyaltycard, 10)).toBe("Serving Coffee (large)");
   });
 
-  it("should serve coffee and change if more money is inserted", () => {
+  it("should serve coffee if exact money is inserted", () => {
+    //Arrange
+    const insertedmoney=2
     const machine = new CoffeeMachine();
-
     const drink = new Drink("Coffee", 2, false, 0, "small");
-
-    expect(machine.serve(drink, 3, false, 10)).toBe("Serving Coffee (small) with change 1.00");
+    //Act and Assert
+    expect(machine.serve(drink, insertedmoney, false, 10)).toBe("Serving Coffee (small)");
   });
-  it("shouldnt serve coffee if enough money isn't inserted", () => {
+
+  it("should serve coffee and give change if more money is inserted", () => {
+    //Arrange
+    const insertedmoney=3
     const machine = new CoffeeMachine();
-
     const drink = new Drink("Coffee", 2, false, 0, "small");
+    //Act and Assert
+    expect(machine.serve(drink, insertedmoney, false, 10)).toBe("Serving Coffee (small) with change 1.00");
+  });
 
-    expect(machine.serve(drink, 1, false, 10)).toBe("Not enough money");
+  it("shouldn't serve coffee if not enough money is inserted", () => {
+    //Arrange
+    const insertedmoney=1
+    const machine = new CoffeeMachine();
+    const drink = new Drink("Coffee", 2, false, 0, "small");
+    //Act and Assert
+    expect(machine.serve(drink, insertedmoney, false, 10)).toBe("Not enough money");
   });
 
   it("should serve coffee with 20% discount during Happy Hour", () => {
+    //Arrange
+    const happyhour=15
+    const insertedmoney=2
     const machine = new CoffeeMachine();
-
     const drink = new Drink("Coffee", 2, false, 0, "small");
-
-    expect(machine.serve(drink, 2, false, 15)).toBe("Serving Coffee (small) with change 0.40");
+    //Act and Assert
+    expect(machine.serve(drink, insertedmoney, false, happyhour)).toBe("Serving Coffee (small) with change 0.40");
   });
-  it("should serve coffee with 20% discount during Happy Hour", () => {
+
+  it("should give change in 2 decimals", () => {
+    //Arrange
+    const insertedmoney=3
     const machine = new CoffeeMachine();
-
     const drink = new Drink("Coffee", 2, false, 0, "small");
-
-    expect(machine.serve(drink, 2, false, 15)).toBe("Serving Coffee (small) with change 0.40");
-  });
-});
-
-describe("Drink", () =>{
-  it("should serve coffee with 20% discount during Happy Hour", () => {
-    const machine = new CoffeeMachine();
-
-    const drink = new Drink("Coffee", 2, false, 0, "small");
-
-    expect(machine.serve(drink, 2, false, 15)).toBe("Serving Coffee (small) with change 0.40");
+    //Act and Assert
+    expect(machine.serve(drink, insertedmoney, false, 10)).toBe("Serving Coffee (small) with change 1.00");
   });
   it("should give error when price of drink is 0 or less", () => {
+    //Arrange
+    const drinkprice=0
     const machine = new CoffeeMachine();
-
-    const drink = new Drink("Coffee", 0, false, 0, "small");
-
-    expect(machine.serve(drink, 2, false, 15)).toBe("Error: invalid price");
+    const drink = new Drink("Coffee", drinkprice, false, 0, "small");
+    //Act and Assert
+    expect(machine.serve(drink, 2, false, 10)).toBe("Error: invalid price");
   });
+
   it("should give error when there are 6 or more sugar cubes", () => {
+    //Arrange
+    const sugarcubes=6
     const machine = new CoffeeMachine();
-
-    const drink = new Drink("Coffee", 2, false, 6, "small");
-
-    expect(machine.serve(drink, 2, false, 15)).toBe("Error: too much sugar");
+    const drink = new Drink("Coffee", 2, false, sugarcubes, "small");
+    //Act and Assert
+    expect(machine.serve(drink, 2, false, 10)).toBe("Error: too much sugar");
   });
-  it("should make price of drink +10 cents for each sugar cube if there are 3 and less than 6", () => {
+
+  it("should add +10 cents to price of drink per sugar cube starting from 3 to 5 sugar cubes", () => {
+    //Arrange
+    const sugarcubes=5
+    const insertedmoney=3
     const machine = new CoffeeMachine();
-
-    const drink = new Drink("Coffee", 2, false, 5, "small");
-
-    expect(machine.serve(drink, 2.30, false, 10)).toBe("Serving Coffee (small)");
+    const drink = new Drink("Coffee", 2, false, sugarcubes, "small");
+    //Act and Assert
+    expect(machine.serve(drink, insertedmoney, false, 10)).toBe("Serving Coffee (small) with change 0.70");
   });
-  it("should make price of drink +10 cents for each sugar cube if there are 3 and less than 6", () => {
+
+  it("shouldn't change price of drink when there are 1 or 2 sugar cubes", () => {
+    //Arrange
+    const sugarcubes=2
+    const insertedmoney=3
     const machine = new CoffeeMachine();
-
-    const drink = new Drink("Coffee", 2, true, 0, "small");
-
-    expect(machine.serve(drink, 2.20, false, 10)).toBe("Serving Coffee (small)");
+    const drink = new Drink("Coffee", 2, false, sugarcubes, "small");
+    //Act and Assert
+    expect(machine.serve(drink, insertedmoney, false, 10)).toBe("Serving Coffee (small) with change 1.00");
   });
-  it("should make price of drink +50 cents when size is medium", () => {
+  it("should add +20 cents to price of drink when there is added milk", () => {
+    //Arrange
+     const insertedmoney=3
+    const addedmilk=true
     const machine = new CoffeeMachine();
-
-    const drink = new Drink("Coffee", 2, false, 0, "medium");
-
-    expect(machine.serve(drink, 2.50, false, 10)).toBe("Serving Coffee (medium)");
+    const drink = new Drink("Coffee", 2, addedmilk, 0, "small");
+    //Act and Assert
+    expect(machine.serve(drink, insertedmoney, false, 10)).toBe("Serving Coffee (small) with change 0.80");
   });
-  it("should make price of drink +1 euro when size is large", () => {
+  it("should add +50 cents to price of drink when size is medium", () => {
+    //Arrange
+    const insertedmoney=3
+    const sizedrink="medium"
     const machine = new CoffeeMachine();
-
-    const drink = new Drink("Coffee", 2, false, 0, "large");
-
-    expect(machine.serve(drink, 3, false, 10)).toBe("Serving Coffee (large)");
+    const drink = new Drink("Coffee", 2, false, 0, sizedrink);
+    //Act and Assert
+    expect(machine.serve(drink, insertedmoney, false, 10)).toBe("Serving Coffee (medium) with change 0.50");
   });
-  
+
+  it("should add +1 euro to price of drink when size is large", () => {
+    //Arrange
+    const insertedmoney=3
+    const sizedrink="large"
+    const machine = new CoffeeMachine();
+    const drink = new Drink("Coffee", 2, false, 0, sizedrink);
+    //Act and Assert
+    expect(machine.serve(drink, insertedmoney, false, 10)).toBe("Serving Coffee (large)");
+  });
 });
